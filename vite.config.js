6d5ebd23ef4c19471b49b319daf867ebc2ffe435
MohammadKhallaf <mohammed.khallaf.1997@gmail.com>
2022-09-13 15:44:42 +0200

    1 binaries uploaded

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
-
+import inject from '@rollup/plugin-inject';
 export default defineConfig({
   optimizeDeps: {
     exclude: ['jquery'],
   },
+  plugins: [
+    // Add it first
+    inject({
+      $: 'jquery',
+    }),
+    // Other plugins...
+  ],
 });
