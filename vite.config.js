ca5498d41eb6894380e40385207ba6543048dc38
MohammadKhallaf <mohammed.khallaf.1997@gmail.com>
2022-09-13 18:21:25 +0200

    1 binaries uploaded

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 export default defineConfig({
   optimizeDeps: {
-    exclude: ['jquery'],
+    exclude: ['jquery', 'owl.carousel'],
   },
   build: {
     rollupOptions: {
