9d084f97cec139f4811aab9b05c5e3af1028585b
MohammadKhallaf <mohammed.khallaf.1997@gmail.com>
2022-09-13 13:56:02 +0200

    added JQuery

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import './style.css';
 import javascriptLogo from './javascript.svg';
-import { setupCounter } from './counter.js';
+import 'jquery';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 
-setupCounter(document.querySelector('#counter'));
-$(document).ready(function(){
+$(document).ready(function () {
   $('.owl-carousel').owlCarousel();
 });
