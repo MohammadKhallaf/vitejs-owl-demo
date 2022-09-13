3b3ea8af76183d3760d502301eb6b0cc1b7856bb
MohammadKhallaf <mohammed.khallaf.1997@gmail.com>
2022-09-13 18:20:26 +0200

    1 binaries uploaded

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 export default defineConfig({
   optimizeDeps: {
     exclude: ['jquery'],
   },
   build: {
     rollupOptions: {
-      external: ['jquery'],
+      external: [path.resolve(__dirname, '/node_modules/jquery')],
     },
   },
 });
